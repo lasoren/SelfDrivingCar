d5f168b6b834c2e875cfaf4a08ba60a17e39a6b5
John Moore <jmoore15@bu.edu>
2014-04-29 00:19:13 -0400

    header

---
+++ SensorCollect.h
@@ -0,0 +1,17 @@
+/*
+ * SensorCollect.h
+ *
+ *  Created on: Apr 28, 2014
+ *      Author: John
+ */
+
+#ifndef SENSORCOLLECT_H_
+#define SENSORCOLLECT_H_
+
+
+
+#endif /* SENSORCOLLECT_H_ */
+
+
+void init_sensors();
+void make_front_measurement();
